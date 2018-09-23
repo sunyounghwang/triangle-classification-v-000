69506809efe7c34749aef00a26bf74ff02a081f0
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-23 21:39:47 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,16 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
 
-  def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 =  side2
-    @side3 = side3
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
-  end
+    case [a,b,c].uniq.size
+    when 1 then :equilateral
+    when 2 then :isosceles
+    else        :scalene
+    end
 end
